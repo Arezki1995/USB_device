bc1ba49dcf9c936805e36439f36a6947fd376723
ailou <anas.ilou@polytech-lille.net>
2018-11-06 15:44:14 +0100

    USB listing devices

--- testUSB.c
+++ testUSB.c
@@ -4,33 +4,43 @@
 
 
 
-int main(){
-
-
+int main()
+	{
+//initialisation
 libusb_context *context;
 int status=libusb_init(&context);
-if(status!=0) {perror("libusb_init"); exit(-1);}
-/* ... some code ... */
+if(status!=0) 
+	{
+	perror("libusb_init"); exit(-1);
+	}
+
+// fin initialisation 
 
 
-
+// code énumération des périphériques USB 
 libusb_device **list;
 ssize_t count=libusb_get_device_list(context,&list);
-if(count<0) {perror("libusb_get_device_list"); exit(-1);}
-ssize_t i=0;
-for(i=0;i<count;i++){
-  libusb_device *device=list[i];
-  struct libusb_device_descriptor desc;
-  int status=libusb_get_device_descriptor(device,&desc);
-  if(status!=0) continue;
-  uint8_t bus=libusb_get_bus_number(device);
-  uint8_t address=libusb_get_device_address(device);
-  printf("Device Found @ (Bus:Address) %d:%d\n",bus,address);
-  printf("Vendor ID 0x0%x\n",desc.idVendor);
-  printf("Product ID 0x0%x\n",desc.idProduct);
-  }
+if(count<0) 
+	{
+		perror("libusb_get_device_list"); exit(-1);
+	}
+	ssize_t i=0;
+for(i=0;i<count;i++)
+      	{
+	  libusb_device *device=list[i];
+	  struct libusb_device_descriptor desc;
+	  int status=libusb_get_device_descriptor(device,&desc);
+	  if(status!=0) 
+		continue;
+	  uint8_t bus=libusb_get_bus_number(device);
+	  uint8_t address=libusb_get_device_address(device);
+	  printf("Device Found @ (Bus:Address) %d:%d\n",bus,address);
+	  printf("Vendor ID 0x0%x\n",desc.idVendor);
+	  printf("Product ID 0x0%x\n",desc.idProduct);
+	  }
 libusb_free_device_list(list,1);
 
+//achevement
 libusb_exit(context);
 
 return 0;
