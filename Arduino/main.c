a27d8f8031db520c6786b83bba720d1e3445448a
Arezki AIT MOUHEB <ait.mouheb.arezki@gmail.com>
2019-01-19 14:58:45 +0100

    Simplest working version of the 328p program

--- Arduino/main.c
+++ Arduino/main.c
@@ -12,19 +12,17 @@ BUTTONS:
 	D3 ---> pin 3 ----> PORTD 3
 	
 	D2 ---> pin 2 ----> PORTD 2
-POTS:
-	Haut/Bas 		--->	Analog in 0 ---> PORTC[0]
-	Gauche/Droite 	--->	Analog in 1 ---> PORTC[1]
 	
-
 */
 // For the serial port
-
 #define CPU_FREQ        16000000L       // Assume a CPU frequency of 16Mhz
-#define SPEED 9600   					//BAUDRATE
+#define BAUD_RATE 9600   					//BAUDRATE
+#define ON_MODE 0x60
+#define OFF_MODE 0x40
+#define _ON 1
+#define _OFF 0
 
-
-uint8_t etatBtn;
+uint8_t etatBtn, old_etatBtn;
 uint8_t etatSerial;
 /////////////////////////////////////////////////////////////////////////
 void init_serial(int speed)
@@ -51,120 +49,94 @@ void send_serial(unsigned char c)
 
 /////////////////////////////////////////////////////////////////////////
 unsigned char get_serial(void) {
-	loop_until_bit_is_set(UCSR0A, RXC0);
+	//loop_until_bit_is_set(UCSR0A, RXC0);
 	return UDR0;
 }
 
-// For I/O handling (examples for PIN 8 as output and PIN 2 as input)
-
-void output_init(void){
+/////////////////////////////////////////////////////////////////////////
+void init_io(){
+	//output
 	//DDRB |= 0x01; // PIN 8 as output
 	DDRB |=0b00111111;
-}
 
-void output_set(unsigned char value){
-	if(value==0) PORTB &= 0xfe; else PORTB |= 0x01;
-}
-
-void input_init(void){
 	//DDRD &= 0xfb;  // PIN 2 as input
-	//PORTD |= 0x04; // Pull-up activated on PIN 2
-	
+	//PORTD |= 0x04; // Pull-up activated on PIN 2	
 	DDRD  &= 0b10000011;
 	PORTD |= 0b01111100;
 }
-
-unsigned char input_get(void){
-	return ((PIND&0x04)!=0)?1:0;
-}
+//////////////////////////////////////////////////////////////////////////
 
 // read Button
 void readBtn(){
-	etatBtn = ( PIND&0x7C ); //0111 1000
+	//etatBtn = ( PIND&0x7C ); //0111 1100
+	etatBtn = ( PIND&=0x7C );  // Lire les pins des boutons
+	etatBtn = (etatBtn >> 2);  // redecaler
+	etatBtn = etatBtn |= 0x20; // ajouter un 1 au bit 5
 }
 
-void readSerial(){
-	etatSerial = UDR0;
-	 //PORTB&0x3F;
-	
+void setLED(uint8_t valeur){
+	if(valeur=='a') PORTB |= 0x01;
+	if(valeur=='b') PORTB |= 0x02;
+	if(valeur=='c') PORTB |= 0x04;
+	if(valeur=='d') PORTB |= 0x08;
+	if(valeur=='e') PORTB |= 0x10;
+	if(valeur=='f') PORTB |= 0x20;
+
+	if(valeur=='A') PORTB &= 0xfe;
+	if(valeur=='B') PORTB &= 0xfd;
+	if(valeur=='C') PORTB &= 0xfb;
+	if(valeur=='D') PORTB &= 0xf7;
+	if(valeur=='E') PORTB &= 0xef;
+	if(valeur=='F') PORTB &= 0xdf;
+
+}
+
+
+void reportBtn(uint8_t etat)
+{	
+	switch(etat){
+		case '7':
+				send_serial('1');
+				break;
+		case ';':
+				send_serial('2');
+				break;
+		case '=':
+				send_serial('3');
+				break;
+		case '>':
+				send_serial('4');
+				break;
+		case '/':
+				send_serial('5');
+				break;
+		default:
+				//send_serial(etat);
+				break;
+	}	
 }
 
 
 int main(void){
 
-	init_serial(SPEED);
-	input_init();
-	output_init();
+	init_serial(BAUD_RATE);	
+	init_io();
+	
 	while(1){
-	etatSerial=get_serial();
-	send_serial(etatSerial);
-	switch (etatSerial){
-			case '0':
-					PORTB=0x00;
-					break;
-			case '1':
-					PORTB|=0x01;	
-					break;
-			case '2':
-					PORTB|=0x02;
-					break;
-			case '3':
-					PORTB|=0x04;	
-					break;
-			case '4':
-					PORTB|=0x08;
-					break;
-			case '5':
-					PORTB|=0x10;	
-					break;
-			case '6':
-					PORTB|=0x20;
-					break;
-			case '7':
-					PORTB=0xFF;	
-					break;	
-			}
-	PORTB=etatSerial;
-	_delay_ms(10);
-	}
-	/*while(1){
+		etatSerial = get_serial();
+		setLED(etatSerial);
+		
 		readBtn();
+		
+			//THIS LINE IS FOR THE MISSING BTN TO REMOVE LATER 
+			etatBtn = etatBtn |= 0x10;
 
-		
-		if ( etatBtn == 0)
-		{	
-			//send_serial('0');
-		}
-		else
-		{
-			
-			switch (etatBtn){
-			case 't':
-					send_serial('D');
-					break;
-			case '\\':
-					send_serial('B');
-					break;
-			case '<':
-					send_serial('G');
-					break;
-			case 'l':
-					send_serial('H');
-					break;	
-			case 'x':
-					send_serial('P');
-					break;										
-			default :
-					send_serial('-');
-			}
-		
-		}
-		_delay_ms(80);
-		
-		etatBtn=0;
-		
+		if(old_etatBtn != etatBtn) {
+			reportBtn( etatBtn);
+			old_etatBtn = etatBtn;
+		}	
+		_delay_ms(20);
+	
 	}
-	*/
-
-	return 0;
+return 0;
 }
